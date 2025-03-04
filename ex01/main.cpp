fd0585529c9666cdfb12da054c164d7abd6a6b18
Aymane Zgaoua <azgaoua@e1r6p6.1337.ma>
2025-03-04 01:53:00 +0000

    done ex01 \!\!

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,17 +1,19 @@
 #include "Serializer.hpp"
 
 int main(){
-    // Serializer k;
-
     Data *ptr = new Data();
     ptr->data_int = 1337;
     std::cout << "---->`" << ptr << "`<----\n";
     std::cout << "---->`" << ptr->data_int << "`<----\n";
-    uintptr_t raw;
+    uintptr_t raw = Serializer::serialize(ptr);
 
-    raw = Serializer::serialize(ptr);
+    std::cout << "---->`0x" << std::hex << raw << std::dec << "`<----\n";
 
-    std::cout << "---->`0x" << std::hex << raw  << "`<----\n";
+    Data *test = Serializer::deserialize(raw);
 
+    std::cout << "---->`" << test << "`<----\n";
+    std::cout << "---->`" << test->data_int <<  "`<----\n";
+
+    delete ptr;
     return (0);
 }
